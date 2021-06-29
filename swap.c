368f01af8ac71e9dee0b777730bfdf1a84b8df1f
me4820 <85018066+me4820@users.noreply.github.com>
2021-06-29 16:22:31 +0530

    mine 2

--- swap.c
+++ swap.c
@@ -17,16 +17,16 @@ void ssort(int ar[], int l)
 
 
 void swap_max(int ar[], int l, int n )
-{  int mx_index=p;
+{  int mx_index=n;
    int tmp;
-   for (int i=p+1; i<l;i++)
+   for (int i=n+1; i<l;i++)
    {
       if(ar[i]>ar[mx_index])
       {
          mx_index=i;
       }
    }
-  tmp=ar[p];
-  ar[p]=ar[mx_index];
+  tmp=ar[n];
+  ar[n]=ar[mx_index];
   ar[mx_index]=tmp;
 }
